44e323c051dd01f293468b26cc65935882417d91
zoro0307 <truongnhuhungnam@gmail.com>
2021-05-16 15:41:36 +0700

    update

--- pages/decks/index.vue
+++ pages/decks/index.vue
@@ -1,11 +1,44 @@
 <template>
-    <div></div>
+    <div>
+        <div class="flex justify-center">
+            <input
+                v-model="deckId"
+                type="text"
+                placeholder="Please enter a deck id"
+                class="border border-gray-600 rounded-sm p-2 mr-1"
+            />
+            <button
+                class="
+                    rounded-sm
+                    px-1
+                    text-white
+                    bg-pink-400
+                    p-2
+                    transition
+                    hover:opacity-50
+                "
+                @click="showDeck"
+            >
+                Go to deck
+            </button>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            deckId: '',
+        }
+    },
     created() {
         this.$store.commit('pagetitle/SET_PAGETITLE', 'DECKS')
+    },
+    methods: {
+        showDeck() {
+            this.$router.push(`/decks/${this.deckId}`)
+        },
     },
 }
 </script>
