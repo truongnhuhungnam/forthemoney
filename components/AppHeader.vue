ca4b756171f4cee54b187efa4dc513ca541ee8b5
zoro0307 <truongnhuhungnam@gmail.com>
2021-05-16 09:38:48 +0700

    update

---
+++ components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-pink-400 py-4">
+        <div class="container mx-auto text-xl text-white">
+            <ul class="flex">
+                <li><nuxt-link to="/">Home</nuxt-link></li>
+                <li><nuxt-link to="/users">Users</nuxt-link></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+ul {
+    li {
+        margin-right: 4em;
+    }
+}
+</style>
--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div></div>
+    <div>{{ $route.params.id }}</div>
 </template>
 
 <script>
